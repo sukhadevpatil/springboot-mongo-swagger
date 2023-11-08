5cb71d05245249c7889ca560008d5fc6761a7704
Sukhdev Patil <sukhadev.patil@gmail.com>
2023-11-08 23:36:06 +0530

    first commit

---
+++ src/main/java/com/learn/nosql/springbootmongoswagger/SpringbootMongoSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.learn.nosql.springbootmongoswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongoSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/nosql/springbootmongoswagger/SpringbootMongoSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.nosql.springbootmongoswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongoSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
